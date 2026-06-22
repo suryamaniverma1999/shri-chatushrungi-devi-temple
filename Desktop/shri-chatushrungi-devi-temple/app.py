0752d45fc95f4d5954f9992dad37fbab3be9052d
Surya Mani <suryamaniverma1999@gmail.com>
2026-06-22 12:40:55 +0530

    Build professional temple website

--- Desktop/shri-chatushrungi-devi-temple/app.py
+++ Desktop/shri-chatushrungi-devi-temple/app.py
@@ -2,21 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+
+@app.route("/timings")
+def timings():
+    return render_template("timings.html")
+
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
 
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
 
-@app.route("/donate")
-def donate():
-    return render_template("donate.html")
+
+@app.route("/donation")
+def donation():
+    return render_template("donation.html")
+
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
